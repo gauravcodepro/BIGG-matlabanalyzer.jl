7871c70ee21ec366fd6da0eb87f48f038b59d7d8
Gaurav <gaurav.sablok@uni-potsdam.de>
2024-06-19 21:04:59 +0200

    2024-6-19
    a julia function to analyze the MATLAB matrix files

--- matlabFBA.m
+++
@@ -1,4 +0,0 @@
-*{
-A matlab function with the graphical user ability and asyncronous programming 
-to analyze the flux based analysis. 
-}*
---
+++ matlabanalyzer.jl
@@ -0,0 +1,40 @@
+#!usr/bin/env julia
+# Author Gaurav
+# Universitat Potsdam
+# Date 2024-6-19
+# A julia function to get the keysvalue search across the matlabfile
+# iterate over the keys and store them into a String[] array and the values as UInt8 bit.
+
+# Pkg.add("MAT")
+using MAT
+
+function readmatlab(matlabfile,description, keysvalue)
+    if matlabfile && description && !keysvalue
+        readfile = read(matopen(matlabfile))
+        matdescription = description
+        for i in keys(readfile[matdescription])
+            println(i)
+        end
+    end
+    if matlabfile && keysvalue
+        readfile = read(matopen(matlabfile))
+        matkeys = keys(readfile[matdescription])
+        matkeysiter = String[]
+        for i in matkeys
+            push!(matkeysiter,i)
+        end
+        matkeysString = ["mets", "grRules", "subSystems", "metFormulas", "metNames", "rxnNames", "rxns", "genes" ]
+        matkeysInt = ["c", "b","rev", "rxnGeneMat", "S", "lb", "metcharge", "ub"]
+        if keyvalue in matkeysString
+            matkeyString = String[]
+            for i in values(readfile[matdescription][keysvalue])
+                push!(matkeyString, i)
+            end
+        end
+        if keyvalue in matkeysInt
+            matkeyString = UInt8[]
+            for i in values(readfile[matdescription][keysvalue])
+                push!(matkeyString, i)
+            end
+        end
+    end
